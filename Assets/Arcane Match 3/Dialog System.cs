4ea262f00f985e0a42bfb30eee7be6cecc9e2e8d
panda <87484491+Hesam3014@users.noreply.github.com>
2025-01-06 11:21:20 -0800

    dialog system fixed

--- Assets/Arcane Match 3/Dialog System.cs
+++ Assets/Arcane Match 3/Dialog System.cs
@@ -1,14 +1,12 @@
 using System;
 using System.Collections;
-using System.Collections.Generic; 
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DialogSystem : MonoBehaviour
 {
-
-
-    [Header("Active Dialogue Param :")]
+    [Header("Active Dialogue Parameters:")]
     public float StartDeley = 5f;
     public GameObject DialogueGameObject;
     public Image CharacterImage;
@@ -16,21 +14,22 @@ public class DialogSystem : MonoBehaviour
 
     [Header("Levels Data")]
     public int CurrentLevel = 0;
-    public int CurrentLevel_Sentece = 0;
+    public int CurrentLevel_Sentence = 0;
     public List<Level> Levels;
-    bool FirstDialog = true;
+    private bool FirstDialog = true;
+
     private void Start()
     {
-        StartCoroutine(ActiveDialoge());
+        StartCoroutine(ActiveDialogue());
     }
+
     private void Update()
     {
         ShowDialogue();
     }
 
-    private IEnumerator ActiveDialoge()
+    private IEnumerator ActiveDialogue()
     {
-
         yield return new WaitForSeconds(StartDeley);
         DialogueGameObject.SetActive(true);
     }
@@ -40,44 +39,42 @@ public class DialogSystem : MonoBehaviour
         if (FirstDialog)
         {
             FirstDialog = false;
-            DialogueText.text = Levels[CurrentLevel].dialogue.Sentence[CurrentLevel_Sentece];
-            CharacterImage.sprite = Levels[CurrentLevel].dialogue.CharacterImage;
+            UpdateDialogueUI();
         }
-        if (DialogueGameObject.activeSelf == true)
+
+        if (DialogueGameObject.activeSelf && Input.GetMouseButtonDown(0))
         {
-
-            if (Input.GetMouseButtonDown(0))
+            if (CurrentLevel_Sentence < Levels[CurrentLevel].dialogue.Sentences.Length - 1)
             {
-                if (CurrentLevel_Sentece < Levels[CurrentLevel].dialogue.Sentence.Length - 1)
-                {
-                    CurrentLevel_Sentece++;
-                    DialogueText.text = Levels[CurrentLevel].dialogue.Sentence[CurrentLevel_Sentece];
-                    CharacterImage.sprite = Levels[CurrentLevel].dialogue.CharacterImage;
-                }
-                else
-                {
-                    DialogueGameObject.SetActive(false);
-                    CurrentLevel_Sentece = 0;
-                }
-
+                CurrentLevel_Sentence++;
+                UpdateDialogueUI();
+            }
+            else
+            {
+                DialogueGameObject.SetActive(false);
+                CurrentLevel_Sentence = 0;
             }
         }
     }
+
+    private void UpdateDialogueUI()
+    {
+        DialogueText.text = Levels[CurrentLevel].dialogue.Sentences[CurrentLevel_Sentence];
+        CharacterImage.sprite = Levels[CurrentLevel].dialogue.CharacterImages[CurrentLevel_Sentence];
+    }
 }
+
 [Serializable]
 public class Level
-{ 
+{
     public Dialogue dialogue;
 }
 
 [Serializable]
 public class Dialogue
 {
-    public Sprite CharacterImage;
     public string CharacterName;
     [TextArea(3, 10)]
-    public string[] Sentence;
+    public string[] Sentences;
+    public Sprite[] CharacterImages;
 }
-
-
-
